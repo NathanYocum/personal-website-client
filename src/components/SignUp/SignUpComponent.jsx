0eacf5b87ef61bd5f5e3d3f340333ebb3c32e220
Nathan Yocum <nayocum@nevada.unr.edu>
2018-06-21 20:22:41 -0700

    Added some components

---
+++ src/components/SignUp/SignUpComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from '../NavBar/NavBarComponent';
+
+class SignUp extends React.Component{
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        
+      </div>
+    )
+  }
+};
